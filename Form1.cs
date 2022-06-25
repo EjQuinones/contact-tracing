9d8215953ed0c91079109a650a24b527b7b4ecb5
Emmanuel John Quiñones <ejquinones2@gmail.com>
2022-06-25 12:45:10 +0800

    errors in configuring the path

--- Form1.cs
+++ Form1.cs
@@ -4,28 +4,14 @@ namespace Quinones_contract_tracing
     {
         public Form1()
         {
-            InitializeComponent();        
+            InitializeComponent();
         }
 
         public object MessageShow { get; private set; }
 
         private void SubmitButton_Click(object sender, EventArgs e)
         {
-            StreamWriter file = new StreamWriter(@"C:\Users\Ej Quinones\Desktop\TRACING - APP DEMO\Contact Tracing Demo.txt");
-            
-                file.Write("");
-                file.WriteLine("Your Name :" + NameTextbox.Text);
-                file.WriteLine("Your Gender :" + GenderTextbox.Text);
-                file.WriteLine("Your Age :" + AgeTextbox.Text);
-                file.WriteLine("Your Address :" + AddressTextbox.Text);
-                file.WriteLine("Date today :" + DateTextbox.Text);
-                file.WriteLine("Temperature :" + TemperatureTextbox.Text);
-                file.WriteLine("Mother's Name :" + MotherTextbox.Text);
-                file.WriteLine("Mother's Occupation :" + MotherOccupationTextbox.Text);
-                file.WriteLine("Father's Name :" + FatherTextbox.Text);
-                file.WriteLine("Father's Occupation :" + FatherOccupationTextbox.Text);
-                file.Close();
-           
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -35,7 +21,7 @@ namespace Quinones_contract_tracing
 
         private void ExitButton_Click(object sender, EventArgs e)
         {
-            this.Close();        
+            this.Close();
         }
 
         private void Respondbutton_Click(object sender, EventArgs e)
@@ -43,6 +29,41 @@ namespace Quinones_contract_tracing
             StreamReader sr = new StreamReader(@"C:\Users\Ej Quinones\Desktop\TRACING - APP DEMO\Contact Tracing Demo.txt");
             Responded.Text = sr.ReadToEnd();
         }
+
+        private void year2022()
+        {
+            StreamWriter file = new StreamWriter(@"C:\Users\Ej Quinones\Desktop\TRACING - APP DEMO\2022\Contact Tracing Demo.txt");
+            file.Write("");
+            file.WriteLine("Your Name :" + NameTextbox.Text);
+            file.WriteLine("Your Gender :" + GenderTextbox.Text);
+            file.WriteLine("Your Age :" + AgeTextbox.Text);
+            file.WriteLine("Your Address :" + AddressTextbox.Text);
+            file.WriteLine("Date today :" + DateTextbox.Text);
+            file.WriteLine("Temperature :" + TemperatureTextbox.Text);
+            file.WriteLine("Mother's Name :" + MotherTextbox.Text);
+            file.WriteLine("Mother's Occupation :" + MotherOccupationTextbox.Text);
+            file.WriteLine("Father's Name :" + FatherTextbox.Text);
+            file.WriteLine("Father's Occupation :" + FatherOccupationTextbox.Text);
+            file.Close();
+        }
+        private void year2023()
+        {
+            StreamWriter file = new StreamWriter(@"C:\Users\Ej Quinones\Desktop\TRACING - APP DEMO\2022\Contact Tracing Demo.txt");
+            file.Write("");
+            file.WriteLine("Your Name :" + NameTextbox.Text);
+            file.WriteLine("Your Gender :" + GenderTextbox.Text);
+            file.WriteLine("Your Age :" + AgeTextbox.Text);
+            file.WriteLine("Your Address :" + AddressTextbox.Text);
+            file.WriteLine("Date today :" + DateTextbox.Text);
+            file.WriteLine("Temperature :" + TemperatureTextbox.Text);
+            file.WriteLine("Mother's Name :" + MotherTextbox.Text);
+            file.WriteLine("Mother's Occupation :" + MotherOccupationTextbox.Text);
+            file.WriteLine("Father's Name :" + FatherTextbox.Text);
+            file.WriteLine("Father's Occupation :" + FatherOccupationTextbox.Text);
+            file.Close();
+            }
+        }    
+        
     }
-}
+
     
